c2e8dfceebe8a33756b7bc87f3a1d18426578d7e
Denis Karpiuk <dzianiskarpiuk@gmail.com>
2021-06-03 10:43:05 +0300

    Add two tets of messagesReducer

--- src/redux/messageReducer.test.js
+++ src/redux/messageReducer.test.js
@@ -1,14 +1,20 @@
 import messageReducer, { addMessage } from './messageReducer'
 
-it('add messages', () => {
-	let state = {
-		messages: [
-			{ id: 1, text: 'Hello' },
-			{ id: 2, text: 'How are you ?' },
-			{ id: 3, text: 'What do you do ?' },
-		],
-	}
-	let action = addMessage('Hello')
+let state = {
+	messages: [
+		{ id: 1, text: 'Hello' },
+		{ id: 2, text: 'How are you ?' },
+		{ id: 3, text: 'What do you do ?' },
+	],
+}
+let action = addMessage('Hello')
+
+it('last message should be correct', () => {
 	let result = messageReducer(state, action)
 	expect(result.messages.length).toBe(4)
+})
+
+it('messages should be add', () => {
+	let result = messageReducer(state, action)
+	expect(result.messages[3].text).toBe('Hello')
 })
