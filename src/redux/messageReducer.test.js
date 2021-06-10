9dc2178c8d3c2dce970075afe37e6cafd13d2503
Denis Karpiuk <dzianiskarpiuk@gmail.com>
2021-06-10 09:35:11 +0300

    Add comment of messageReducerTest

--- src/redux/messageReducer.test.js
+++ src/redux/messageReducer.test.js
@@ -1,5 +1,6 @@
 import messageReducer, { addMessage } from './messageReducer'
-
+//! Тестируем фунцкию Reducer
+//* Создаём начальный state для reducer
 let state = {
 	messages: [
 		{ id: 1, text: 'Hello' },
@@ -7,14 +8,19 @@ let state = {
 		{ id: 3, text: 'What do you do ?' },
 	],
 }
+//* Вызваем AC для reducer и передаём в него данные
 let action = addMessage('Hello')
-
-it('last message should be correct', () => {
+//* Создаём тест, который проверяет корректность
+it('messages should be add', () => {
+	//* Получаем результат работы функции, вызвав её с переданными в неё параметрами
 	let result = messageReducer(state, action)
+	//* Проверяем результат на соотвтествие ожиданиям
 	expect(result.messages.length).toBe(4)
 })
-
-it('messages should be add', () => {
+//* Проверяем что сообщение было добавлено
+it('message text shoul be correct', () => {
+	//* Резульатат вызова функции
 	let result = messageReducer(state, action)
+	//* Проверяем результат
 	expect(result.messages[3].text).toBe('Hello')
 })
