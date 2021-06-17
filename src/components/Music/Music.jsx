1ab113c8627babcd020f17e647231384c5420b49
Denis Karpiuk <dzianiskarpiuk@gmail.com>
2021-06-17 16:39:35 +0300

    Refactoring Music component

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -1,72 +1,13 @@
 import React from 'react'
+import HeaderPage from '../Common/HeaderPage/HeaderPage'
+import musicBg from '../../assets/images/BackgroundsHeaders/bg3.jpg'
 import s from './Music.module.css'
 
 const Music = props => {
-	if (props.tracks.length === 0) {
-		props.setTracks([
-			{
-				id: 1,
-				trackName: 'Hello',
-				artist: 'Cue',
-				preView:
-					'https://img2.freepng.ru/20180402/jiw/kisspng-computer-icons-youtube-play-button-clip-art-play-button-5ac1d4d234ce28.5671524315226523702163.jpg',
-				times: '3.56',
-			},
-			{
-				id: 2,
-				trackName: 'Beautiful life',
-				artist: 'Thirty second to mars',
-				preView:
-					'https://img2.freepng.ru/20180402/jiw/kisspng-computer-icons-youtube-play-button-clip-art-play-button-5ac1d4d234ce28.5671524315226523702163.jpg',
-				times: '5.06',
-			},
-			{
-				id: 3,
-				trackName: 'Stop and Play',
-				artist: 'Justin Beebier',
-				preView:
-					'https://img2.freepng.ru/20180402/jiw/kisspng-computer-icons-youtube-play-button-clip-art-play-button-5ac1d4d234ce28.5671524315226523702163.jpg',
-				times: '4.00',
-			},
-			{
-				id: 4,
-				trackName: 'Long Music',
-				artist: 'IzzaMusic',
-				preView:
-					'https://img2.freepng.ru/20180402/jiw/kisspng-computer-icons-youtube-play-button-clip-art-play-button-5ac1d4d234ce28.5671524315226523702163.jpg',
-				times: '50.56',
-			},
-		])
-	}
 	return (
 		<div className={s.musicPage}>
-			<div className={s.pageTitle}>{props.pageName}</div>
-			<div iv className={s.tracks}>
-				{props.tracks.map((track, index) => (
-					<div className={s.track} key={track.id}>
-						<div className={s.preView} onClick={() => props.playTrack(index)}>
-							<img src={track.preView} />
-						</div>
-						<div className={s.test}>
-							<div className={s.trackName}>{track.trackName}</div>
-							<div className={s.artist}>{track.artist}</div>
-						</div>
-
-						<div className={s.trackInfo}>
-							<div className={s.timeAndButton}>
-								<div className={s.times}>{track.times}</div>
-								<div className={s.time_button}>
-									<div className={s.button}>
-										<button onClick={() => props.deleteTrack(index)}>
-											delete
-										</button>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				))}
-			</div>
+			<HeaderPage img={musicBg} tittle={'Music'} />
+			<div className={s.content}>in progress...</div>
 		</div>
 	)
 }
--- src/redux/Music-Recucer.js
+++ src/redux/Music-Recucer.js
@@ -1,53 +1,49 @@
-const PLAY = 'PLAY';
-const DELETE = 'DELETE';
-const SETTRACKS = 'SETTRACKS';
-
+const PLAY = 'PLAY'
+const DELETE = 'DELETE'
+const SETTRACKS = 'SETTRACKS'
 
 const initialState = {
-    pageName: 'Музыка',
-    tracks: []
+	tracks: [],
 }
-
 
 const musicReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case PLAY:
-            alert(state.tracks[action.index].trackName);
-            return state
-        case DELETE:
-            return {
-                ...state,
-                deleteTrack: [...state.tracks.splice(action.index, 1)],
-                tracks: [...state.tracks]
-            }
-        case SETTRACKS:
-            return {
-                ...state, tracks: [...state.tracks, ...action.tracks]
-            }
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case PLAY:
+			alert(state.tracks[action.index].trackName)
+			return state
+		case DELETE:
+			return {
+				...state,
+				deleteTrack: [...state.tracks.splice(action.index, 1)],
+				tracks: [...state.tracks],
+			}
+		case SETTRACKS:
+			return {
+				...state,
+				tracks: [...state.tracks, ...action.tracks],
+			}
+		default:
+			return state
+	}
 }
 
-
-
-export const playTrackAC = (index) => {
-    return {
-        type: PLAY,
-        index: index
-    }
+export const playTrackAC = index => {
+	return {
+		type: PLAY,
+		index: index,
+	}
 }
-export const deleteTrackAC = (index) => {
-    return {
-        type: DELETE,
-        index: index
-    }
+export const deleteTrackAC = index => {
+	return {
+		type: DELETE,
+		index: index,
+	}
 }
-export const setTracksAC = (tracks) => {
-    return {
-        type: SETTRACKS,
-        tracks: tracks
-    }
+export const setTracksAC = tracks => {
+	return {
+		type: SETTRACKS,
+		tracks: tracks,
+	}
 }
 
-export default musicReducer;
+export default musicReducer
